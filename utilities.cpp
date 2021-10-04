c920766b6b676901536d390a00a8b975f2bc4454
kahou76 <90814909+kahou76@users.noreply.github.com>
2021-10-03 21:45:43 -0700

    Create utilities.cpp

---
+++ utilities.cpp
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author KA HOU CHAN
+ * @date   2021-10-03
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+// defined in utilities.cpp
+void helper() { cout << "Testing multi-file compile/run" << endl; }
